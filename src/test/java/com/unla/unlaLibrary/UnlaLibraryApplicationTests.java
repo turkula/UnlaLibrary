1ab14b86d3e9f6d202cd12d095b10cd6a84fae7a
Claudio Matias Turkula <claudioturkula@hotmail.com>
2021-05-09 11:49:30 -0300

    add spring

---
+++ src/test/java/com/unla/unlaLibrary/UnlaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.unlaLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
